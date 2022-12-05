582d8cd84d7839058b569fe1db66dd95e9a9da2d
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2022-12-05 15:01:32 +0100

    made some chnages

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -125,7 +125,7 @@ const Weather = ({latitude, longitude}) => {
       try {
         const responses = await api.get("/current.json", {
           params: {
-            q: `${latitude},${longitude}` || currentCity || "auto:ip",
+            q:  "auto:ip",
           },
         });
         if (responses) {
