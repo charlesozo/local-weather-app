1f730667b05e90818341ad01f1789e0f52afcf0d
Charles Ozochukwu <charlesozochukwu2004@gmail.com>
2022-12-17 22:20:16 +0100

    changed token

--- src/components/api.jsx
+++ src/components/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const TOKEN = "800f69ea8a204c249a1155233220712";
+const TOKEN = "472c154ffe7c4e8988f210324221712";
 
 export default axios.create({
   baseURL: " https://api.weatherapi.com/v1",
